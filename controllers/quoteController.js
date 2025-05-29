8e3efc1f6ed7d7c0dab2001d142574c433bf197a
abhishek singh <ascs739@gmail.com>
2025-05-29 15:01:23 +0530

    added file and pagination in quotes api

--- controllers/quoteController.js
+++ controllers/quoteController.js
@@ -7,7 +7,7 @@ export const addQuote = asyncHandler(async (req, res) => {
   const { title, content } = req.body;
   if (!title) throw new AppError("Title is required", 400);
 
-  const imagePath = req.file ? `/uploads/${req.file.filename}` : "";
+  const imagePath = req.fileUrl || "";
 
   const quote = await Quote.create({
     title,
@@ -23,8 +23,24 @@ export const addQuote = asyncHandler(async (req, res) => {
 
 // Get all quotes
 export const getAllQuotes = asyncHandler(async (req, res) => {
-  const quotes = await Quote.find().sort({ createdAt: -1 });
-  res.status(200).json({ count: quotes.length, quotes });
+  const page = parseInt(req.query.page) || 1; // Default to page 1
+  const limit = parseInt(req.query.limit) || 10; // Default to 10 items per page
+  const skip = (page - 1) * limit;
+
+  const quotes = await Quote.find()
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit);
+
+  const totalQuotes = await Quote.countDocuments();
+
+  res.status(200).json({
+    count: quotes.length,
+    total: totalQuotes,
+    page,
+    totalPages: Math.ceil(totalQuotes / limit),
+    quotes,
+  });
 });
 
 // Get quote by ID
@@ -39,7 +55,7 @@ export const updateQuote = asyncHandler(async (req, res) => {
   const updates = req.body;
 
   if (req.file) {
-    updates.image = `/uploads/${req.file.filename}`;
+    updates.image = req.fileUrl;
   }
 
   const updatedQuote = await Quote.findByIdAndUpdate(
