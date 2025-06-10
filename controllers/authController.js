70364a4086286d65531a24f40c8dfda539b47d08
abhishektw456 <abhishek.singh@thoughtwin.com>
2025-06-10 17:55:30 +0530

    id to _id in login api

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,7 +45,7 @@ const login = async (req, res) => {
     message: "Login successful",
     token,
     user: {
-      id: user._id,
+      _id: user._id,
       name: user.name,
       email: user.email,
     },
