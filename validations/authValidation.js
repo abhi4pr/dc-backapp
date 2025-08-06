79ba59ef75325eec04e4ec5549cf39091c4c67f8
abhishektw456 <abhishek.singh@thoughtwin.com>
2025-08-06 13:55:48 +0530

    removed role from validation

--- validations/authValidation.js
+++ validations/authValidation.js
@@ -4,7 +4,6 @@ export const signupSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-  role: Joi.string().required(),
 });
 
 export const loginSchema = Joi.object({
